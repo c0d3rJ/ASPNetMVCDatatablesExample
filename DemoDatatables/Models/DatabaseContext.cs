aa1f4a352c06e52b2e977f621d98f1f24dd60279
c0d3r <47030532+c0d3rJ@users.noreply.github.com>
2021-05-20 06:40:08 -0400

    Add project files.

---
+++ DemoDatatables/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DemoDatatables.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DBConnection")
+        {
+
+        }
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
